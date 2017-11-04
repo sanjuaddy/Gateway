4a6ff5f2ef8339cec925d479ee36f3dd05032894
sanjuaddy <sanjuaddy@gmail.com>
2017-11-04 14:28:44 +0530

    Config Testlogin Change

--- src/test/java/bg/testScripts/login/TestLogin.java
+++ src/test/java/bg/testScripts/login/TestLogin.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import org.testng.annotations.AfterClass;
 
 public class TestLogin extends TestBase {
+	String sheetName = "TestLogin";
   @BeforeClass
   public void setUp() throws IOException {
 	  init();
@@ -22,6 +23,7 @@ public class TestLogin extends TestBase {
   public void testLogin() throws Exception {
 	LoginPage login = new LoginPage();
 	login.loginToApplication();
+	captureScreenshot(sheetName);
   }
   
   @AfterClass
